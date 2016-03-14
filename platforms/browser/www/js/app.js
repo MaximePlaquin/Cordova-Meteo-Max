327b860b882d39b2336fbdb3698a706c9af14b05
Maxime <plaquin.maxime@gmail.com>
2016-03-14 23:04:34 +0100

    Ajout de L'API
    Ajout de l’api openweathermap

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('deviceready', function(){
+
+}, false);
+var app = angular.module('app', []);
+
+app.config(function($routeProvider){
+
+  $routeProvider
+    .when('/home', {templateUrl: 'partials/home.html'})
+    .when('/about', {templateUrl: 'partials/about.html'})
+    .otherwise({redirectTo: '/home'})
+
+})
